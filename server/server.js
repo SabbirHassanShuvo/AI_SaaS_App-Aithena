da17876898c3f3b6ba69dae31ef2649919aaefeb
SabbirHassanShuvo <sabbirhassanshuvo71@gmail.com>
2025-12-22 06:22:03 +0600

    Backend Update

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
+import dotenv from "dotenv";
 import app from "./app.js";
 import cloudinary from "cloudinary";
+import connectDB from "./database/dbConnection.js";
+
+dotenv.config();
+
+// database connect
+connectDB();
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRECT,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.listen(process.env.PORT, () => {
-  console.log("Server is running on ${process.env.PORT}");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
 });
